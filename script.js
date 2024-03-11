3790a7ca1370ae2e68a0197f51b7698dacdcadd0
Oliver Delaney <oliver.delaney@stu.barren.kyschools.us>
2024-03-11 09:55:02 -0500

    Created script.js file. Created starting game variables.

---
+++ script.js
@@ -0,0 +1,15 @@
+// GUESS THAT NUMBER
+// Message to be used throughout the project/file
+const enterNumText = `Please enter a number greater than zero`;
+// For starting, restarting, and ending the game
+let restartGame = true;
+// For storing the range of the number to be guessed
+let rangeNum;
+// For storing the number that the user will attempt to guess
+let randomNum;
+// For storing the number of attempts the user has left
+let attempts;
+// For storing the user's guess
+let guess;
+// For storing the user's response to play again or not play again
+let playAgain;
